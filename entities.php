98308759ec57e30e10b7933a1e74ccfdd2148c93
Mark Dvoryanchikov <s-punk-a-m@mail.ru>
2017-04-12 15:51:52 +0700

    returnurl fixes2

--- entities.php
+++ entities.php
@@ -32,7 +32,7 @@ for ($i = 0; $i < count($entities); $i++) {
 
 $model = get_base_model();
 $model['title'] = 'Список элементов';
-$model['returnurl'] = "{$model['current url']}#main";
+$model['returnurl'] = "{$model['current_url']}#main";
 $model['context'] = $context;
 $model['entities'] = $objects;
 
